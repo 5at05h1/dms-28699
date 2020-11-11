af1a8234e7ad9209f210eb3b66affe6c846e3112
5at05h1 <dfcbvghl_3104@icloud.com>
2020-11-11 15:50:38 +0900

    モデル、ルーティング記述

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+  $items = Item::orderBy('id', 'desc')->get();
+
+  return view('index', [
+      'items' => $items
+  ]);
 });
